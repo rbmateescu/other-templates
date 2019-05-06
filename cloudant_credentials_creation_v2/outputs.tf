ea6707709d365c25deb17df070b0f0176161edd1
mateescu <mateescu@ca.ibm.com>
2019-05-06 17:05:32 -0400

    result

--- cloudant_credentials_creation_v2/outputs.tf
+++ cloudant_credentials_creation_v2/outputs.tf
@@ -6,41 +6,33 @@
 
 
 output "apikey" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.apikey.result.stdout}"
+  value = "${camc_scriptpackage.apikey.result["stdout"]}"
 }
 
 output "host" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.host.result.stdout}"
+  value = "${camc_scriptpackage.host.result["stdout"]}"
 }
 
 output "iam_apikey_description" {
-  #value = "${data.external.com_output.result["apikey"]}"
   value = "${camc_scriptpackage.iam_apikey_description.result["stdout"]}"
 }
 
 output "iam_apikey_name" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.iam_apikey_name.result.stdout}"
+  value = "${camc_scriptpackage.iam_apikey_name.result["stdout"]}"
 }
 
 output "password" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.password.result.stdout}"
+  value = "${camc_scriptpackage.password.result["stdout"]}"
 }
 
 output "port" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.port.result.stdout}"
+  value = "${camc_scriptpackage.port.result["stdout"]}"
 }
 
 output "url" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.url.result.stdout}"
+  value = "${camc_scriptpackage.url.result["stdout"]}"
 }
 
 output "username" {
-  #value = "${data.external.com_output.result["apikey"]}"
-  value = "${camc_scriptpackage.username.result.stdout}"
+  value = "${camc_scriptpackage.username.result["stdout"]}"
 }
